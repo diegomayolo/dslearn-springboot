aeb357a517b4f97fb2bc3dc383c63058e93ff6f9
Diego Luis Mayolo <diego.mayolo@outlook.com>
2023-05-17 21:26:01 -0300

    Resource entity and relational mapping Offer/Resource

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -17,3 +17,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ( 'Java', 'https://en
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_Id) VALUES ( '1.0', '2023-01-20T03:00:00Z', '2024-01-20T03:00:00Z', 1 );
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_Id) VALUES ( '2.0', '2023-05-20T03:00:00Z', '2024-05-20T03:00:00Z', 1 );
+
+INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_Id) VALUES ('Trilha Spring Boot', 'Trilha principal do curso', 1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA4JuXTPJLdp9Ek6TDpjZLV_2lOEbF2D-MIw&usqp=CAU', 1, 1);
+INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_Id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA4JuXTPJLdp9Ek6TDpjZLV_2lOEbF2D-MIw&usqp=CAU', 2, 1);
+INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_Id) VALUES ('Lives', 'Lives exclusivas para a turam', 3, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA4JuXTPJLdp9Ek6TDpjZLV_2lOEbF2D-MIw&usqp=CAU', 0, 1);
