4adfea4137e9a682d596484621c59e33ddbb04b0
Diego Luis Mayolo <diego.mayolo@outlook.com>
2023-05-22 21:38:24 -0300

    Reply entity; reply likes, object mapping and seed in database

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Topic.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Topic.java
@@ -3,9 +3,7 @@ package com.devsuperior.dslearnbds.entities;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table( name = "tb_topic" )
@@ -36,11 +34,18 @@ public class Topic implements Serializable
     @JoinColumn( name = "lesson_id" )
     private Lesson lesson;
 
+    @ManyToOne
+    @JoinColumn( name = "reply_id" )
+    private Reply answer;
+
     @ManyToMany
     @JoinTable( name = "tb_topic_likes",
                 joinColumns = @JoinColumn( name = "topic_id" ),
                 inverseJoinColumns = @JoinColumn( name = "user_id" ) )
     private Set<User> likes = new HashSet<>();
+
+    @OneToMany( mappedBy = "topic" )
+    private List<Reply> replies = new ArrayList<>();
 
     public Topic() {
     }
@@ -113,6 +118,18 @@ public class Topic implements Serializable
 
     public Set<User> getLikes() {
         return likes;
+    }
+
+    public Reply getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Reply answer) {
+        this.answer = answer;
+    }
+
+    public List<Reply> getReplies() {
+        return replies;
     }
 
     @Override
